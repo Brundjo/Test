6904b0cbc13381cbf76c9fcbc42b8ef4c7baf27c
Your Name <you@example.com>
2022-02-02 05:21:18 +0300

    class

--- auth.php
+++ auth.php
@@ -4,112 +4,98 @@
 
     //Добавляем файл подключения к БД
     require_once("dbconnect.php");
-    /*
-    Проверяем была ли отправлена форма с помощью AJAX. Если да, то идём дальше, если нет, то выведем пользователю сообщение об ошибке, о том что он зашёл на эту страницу напрямую.
-    */
-	if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-        
-        //(1) Login
+	
+	class User {
 		
-        //Обрезаем пробелы с начала и с конца строки
-        $login = trim($_POST["login"]);
-        if(isset($_POST["login"])){
-             
-            if(!empty($login)){
-                $login = htmlspecialchars($login, ENT_QUOTES);
-             
-                //Проверяем формат полученного логина с помощью регулярного выражения
-                $reg_login = "(.{6,})";
-             
-                //Если формат полученного логина не соответствует регулярному выражению
-                if( !preg_match($reg_login, $login)){
-					// Сообщение об ошибке.
-					$result = 'Вы ввели неправильный логин';
-                    echo json_encode($result);
-                    //Останавливаем скрипт
-                    exit();
-                }
-            }else{
-                // Сообщение об ошибке.
+		public function __construct(){
+			$this->login = trim($_POST["login"]);
+			$this->password = trim($_POST["password"]);
+			$this->name = null;
+			$this->z = 0;
+		}
+	}
+	
+	class Message {
+		
+		public static function checks(){
+			global $jsonArray;
+			
+		//Проверяем была ли отправлена форма с помощью AJAX. Если да, то идём дальше, если нет, значит пользователь зашёл на эту страницу напрямую. В этом случае выводим ему сообщение об ошибке.
+			
+			if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) || empty($_SERVER['HTTP_X_REQUESTED_WITH']) || !strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+				exit("<p><strong>Ошибка!</strong> Вы зашли на эту страницу напрямую, поэтому нет данных для обработки. Вы можете перейти на <a href='index.php'> главную страницу </a>.</p>");
+			}
+			
+			if (!isset($_POST["login"]) || !isset($_POST["password"])) {
+				// Сообщение об ошибке. 
+				$result = 'Отсутствует поле для ввода данных';
+				echo json_encode($result);
+				//Останавливаем скрипт
+				exit();
+			}
+			
+			$user = new User();
+			
+		//(1) Login
+		
+			if (empty($user->login)) {
+				// Сообщение об ошибке. 
 				$result = 'Поле для ввода логина не должна быть пустым.';
-                echo json_encode($result);
-                //Останавливаем скрипт
-                exit();
-            }
-                 
-        }else{
-            // Сообщение об ошибке. 
-			$result = 'Отсутствует поле для ввода логина';
-            echo json_encode($result);
-            //Останавливаем скрипт
-            exit();
-        }
-             
-        //(2) Password
-        if(isset($_POST["password"])){
- 
-            //Обрезаем пробелы с начала и с конца строки
-            $password = trim($_POST["password"]);
-             
-            if(!empty($password)){
-                $password = htmlspecialchars($password, ENT_QUOTES);
-             
-                //Шифруем пароль
-                $password = md5($password."top_secret");
-            }else{
-                // Сообщение об ошибке. 
+				echo json_encode($result);
+				//Останавливаем скрипт
+				exit();
+			}
+			
+			if (!preg_match("(\S{6,})", $user->login)) {
+				// Сообщение об ошибке. 
+				$result = 'Вы ввели неправильный логин.';
+				echo json_encode($result);
+				//Останавливаем  скрипт
+				exit();
+			}
+			
+		//(2) Password	
+			
+			if (empty($user->password)) {
+				// Сообщение об ошибке. 
 				$result = 'Укажите Ваш пароль';
 				echo json_encode($result);
-                //Останавливаем скрипт
-                exit();
-            }
-        }else{
-            // Сообщение об ошибке. 
-			$result = 'Отсутствует поле для ввода логина';
-			echo json_encode($result);
-            //Останавливаем скрипт
-            exit();
-        }
-
-        //(3) Cоставления запроса к БД
-        //Запрос в БД на выборке пользователя.
-           
-		$z = 0;
-		$login;
-		$name;
+				//Останавливаем скрипт
+				exit();
+			}
 			
-		function recursion($arr){
+			Db::read($user, $user->password, $jsonArray);
+		}
+	}
+		
+	class Db {
+		
+		public static function recursion($arr, $user){
 			foreach ($arr as $key){
-				global $login;
-				global $password;
+				$login = $user->login;
+				$password = $user->password;
 				if ($key["login"] == $login && $key["password"] == $password){
-					global $z;
-					$z++;
-					global $name;
-					$name = $key["name"];
+					$user->z++;
+					$user->name = $key["name"];
 				} 
 			}
 		}
-					
-		recursion($jsonArray);
+		
+		public static function read($user, $pass, $jsonArray){
+			//Шифруем пароль
+			$user->password = md5($pass."top_secret");
 			
-        if(!$z && $z != 0){
-            // Сообщение об ошибке. 
-			$result = 'Ошибка запроса на выборке пользователя из БД';
-			echo json_encode($result);
-            //Останавливаем скрипт
-            exit();
-        }else{
+			Db::recursion($jsonArray, $user);
+			
             //Проверяем, если в базе нет пользователя с такими данными, то выводим сообщение об ошибке
-            if($z == 1){
+            if($user->z == 1){
                 // Если введенные данные совпадают с данными из базы, то сохраняем логин, пароль и имя в массив сессий.
-                $_SESSION['login'] = $login;
-                $_SESSION['password'] = $password;
-				$_SESSION['name'] = $name;
+                $_SESSION['login'] = $user->login;
+                $_SESSION['password'] = $user->password;
+				$_SESSION['name'] = $user->name;
 				
 				$result = 1;
 				echo json_encode($result);
-				
 				//Останавливаем скрипт
                 exit();
             }else{
@@ -119,7 +105,9 @@
                 //Останавливаем скрипт
                 exit();
             }
-        }
-    }else{
-        exit("<p><strong>Ошибка!</strong> Вы зашли на эту страницу напрямую, поэтому нет данных для обработки. Вы можете перейти на <a href='index.php'> главную страницу </a>.</p>");
-    }
+		}
+	}
+	
+	Message::checks();
+
+?>
--- register.php
+++ register.php
@@ -4,289 +4,209 @@
 
     //Добавляем файл подключения к БД
     require_once("dbconnect.php");
-    /*
-        Проверяем была ли отправлена форма с помощью AJAX. Если да, то идём дальше, если нет, значит пользователь зашёл на эту страницу напрямую. В этом случае выводим ему сообщение об ошибке.
-	*/    
-	if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-
-		// (1) Login
-
-        // Проверяем если в глобальном массиве $_POST существуют данные отправленные из формы и заключаем переданные данные в обычные переменные.
-        if(isset($_POST["login"])){
-                
-			//Обрезаем пробелы с начала и с конца строки
-            $login = trim($_POST["login"]);
-
-            //Проверяем переменную на пустоту
-            if(!empty($login)){
-                // Для безопасности, преобразуем специальные символы в HTML-сущности
-                $login = htmlspecialchars($login, ENT_QUOTES);
-					
-				$reg_login = "(\S{6,})";
-
-                //Если формат полученного логин не соответствует регулярному выражению
-                if( !preg_match($reg_login, $login)){
-                    // Сообщение об ошибке. 
-					$result = 'Вы ввели неправильный логин. Минимум 6 символов, не должно присутствовать пробельных символов';
-                    echo json_encode($result);
-						
-                    //Останавливаем  скрипт
-                    exit();
-                }
-					
-				$z = 0;
-				function recurs($arr){
-					foreach ($arr as $key => $val) {
-						global $login;
-						$type = gettype($val);
-						if( $type == 'object' || $type == 'array'){
-							recurs($val);
-						} else if ($key == 'login' && $val == $login){
-							global $z;
-							$z++;
-						}
-					}
+	
+	class User {
+		
+		public function __construct(){
+			$this->login = trim($_POST["login"]);
+			$this->name = trim($_POST["name"]);
+			$this->email = trim($_POST["email"]);
+			$this->confirm_password = trim($_POST["confirm_password"]);
+			$this->password = trim($_POST["password"]);
+			$this->z = 0;
+			$this->e = 0;
+		}
+	}
+	
+	class Message {	
+	
+		public static function recurs($arr, $user){
+			foreach ($arr as $key => $val) {
+				$login = $user->login;
+				$type = gettype($val);
+				if( $type == 'object' || $type == 'array'){
+					Message::recurs($val, $user);
+				} else if ($key == 'login' && $val == $login){
+					$user->z = 1;
 				}
-				recurs($jsonArray);
-                    
-                //Если кол-во полученных строк ровно единице, значит пользователь с таким логином уже зарегистрирован
-                if($z >= 1){
-                    //Если полученный результат не равен false
-                    if($z != false){
-                        // Сообщение об ошибке. 
-						$result = 'Пользователь с таким логином уже зарегистрирован';
-                    }else{
-                        // Сообщение об ошибке. 
-						$result = 'Ошибка в запросе к БД';
-                    }
-					echo json_encode($result);
-                    //Останавливаем  скрипт
-                    exit();
-                }
-					
-            }else{
-                // Сообщение об ошибке. 
+			}
+		}
+				
+		public static function recursion($arr, $user){
+			foreach ($arr as $key => $val) {
+				$email = $user->email;
+				$type = gettype($val);
+				if( $type == 'object' || $type == 'array'){
+					Message::recursion($val, $user);
+				} else if ($key == 'email' && $val == $email){
+					$user->e = 1;
+				}
+			}
+		}
+	
+		public static function checks(){
+			global $jsonArray;
+			
+		//Проверяем была ли отправлена форма с помощью AJAX. Если да, то идём дальше, если нет, значит пользователь зашёл на эту страницу напрямую. В этом случае выводим ему сообщение об ошибке.
+			
+			if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) || empty($_SERVER['HTTP_X_REQUESTED_WITH']) || !strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+				exit("<p><strong>Ошибка!</strong> Вы зашли на эту страницу напрямую, поэтому нет данных для обработки. Вы можете перейти на <a href='index.php'> главную страницу </a>.</p>");
+			}
+			
+			if (!isset($_POST["login"]) || !isset($_POST["name"]) || !isset($_POST["email"]) || !isset($_POST["confirm_password"]) || !isset($_POST["password"])) {
+				// Сообщение об ошибке. 
+				$result = 'Отсутствует поле для ввода данных';
+				echo json_encode($result);
+				//Останавливаем скрипт
+				exit();
+			}
+			
+			$user = new User();
+			
+		// (1) Login
+		
+			if (empty($user->login)) {
+				// Сообщение об ошибке. 
 				$result = 'Укажите Ваш логин';
 				echo json_encode($result);
-                //Останавливаем скрипт
-                exit();
-            }
-		   
-        }else{
-            // Сообщение об ошибке.
-			$result = 'Отсутствует поле с логином';
-			echo json_encode($result);
-               
-            //Останавливаем скрипт
-            exit();
-        }
-
-        // (2) Name
-		
-        if(isset($_POST["name"])){
-
-            //Обрезаем пробелы с начала и с конца строки
-            $name = trim($_POST["name"]);
-
-            if(!empty($name)){
-                // Для безопасности, преобразуем специальные символы в HTML-сущности
-                $name = htmlspecialchars($name, ENT_QUOTES);
-				$reg_name = "/^[a-zA-Z]{2}$/";
-
-                //Если формат полученного имени не соответствует регулярному выражению
-                if( !preg_match($reg_name, $name)){
-                    // Сообщение об ошибке. 
-                    $result = 'Вы ввели неправильное имя';
-					echo json_encode($result);
-                    //Останавливаем  скрипт
-                    exit();
-                }
-					
-            }else{
-                // Сообщение об ошибке. 
-                $result = 'Укажите Ваше имя';
+				//Останавливаем скрипт
+				exit();
+			}
+			
+			if (!preg_match("(\S{6,})", $user->login)) {
+				// Сообщение об ошибке. 
+				$result = 'Вы ввели неправильный логин. Минимум 6 символов, не должно присутствовать пробельных символов';
 				echo json_encode($result);
-                //Останавливаем  скрипт
-                exit();
-            }
-
-        }else{
-            // Сообщение об ошибке. 
-            $result = 'Отсутствует поле с именем';
-			echo json_encode($result);
-            //Останавливаем  скрипт
-            exit();
-        }
-		
+				//Останавливаем  скрипт
+				exit();
+			}
+			
+			Message::recurs($jsonArray, $user);
+			if ($user->z >= 1) {
+				// Сообщение об ошибке. 
+				$result = 'Пользователь с таким логином уже зарегистрирован';
+				echo json_encode($result);
+				//Останавливаем  скрипт
+				exit();
+			}
+			
+		// (2) Name
+			
+			if (empty($user->name)) {
+				// Сообщение об ошибке. 
+				$result = 'Укажите Ваше имя';
+				echo json_encode($result);
+				//Останавливаем скрипт
+				exit();
+			}
+			
+			if (!preg_match("/^[a-zA-Z]{2}$/", $user->name)) {
+				// Сообщение об ошибке. 
+				$result = 'Вы ввели неправильное имя';
+				echo json_encode($result);
+				//Останавливаем  скрипт
+				exit();
+			}
+			
 		// (3) Email
-		
-        if(isset($_POST["email"])){
-            //Обрезаем пробелы с начала и с конца строки
-            $email = trim($_POST["email"]);
-
-            if(!empty($email)){
-                $email = htmlspecialchars($email, ENT_QUOTES);
-
-                //Проверяем формат полученного почтового адреса с помощью регулярного выражения
-                $reg_email = "/^[a-z0-9][a-z0-9\._-]*[a-z0-9]*@([a-z0-9]+([a-z0-9-]*[a-z0-9]+)*\.)+[a-z]+/i";
-
-                //Если формат полученного почтового адреса не соответствует регулярному выражению
-                if( !preg_match($reg_email, $email)){
-                    // Сообщение об ошибке.
-                    $result = 'Вы ввели неправельный email';
-					echo json_encode($result);
-                    //Останавливаем  скрипт
-                    exit();
-                }
-
-                //Проверяем нет ли уже такого адреса в БД.
-				$z = 0;
-				function recursion($arr){
-					foreach ($arr as $key => $val) {
-						global $email;
-						$type = gettype($val);
-						if( $type == 'object' || $type == 'array'){
-							recursion($val);
-						} else if ($key == 'email' && $val == $email){
-							global $z;
-							$z++;
-						}
-					}
-				}
-				recursion($jsonArray);
-					
-                //Если кол-во полученных строк равно единице, значит пользователь с таким почтовым адресом уже зарегистрирован
-                if($z >= 1){
-                    //Если полученный результат не равен false
-                    if(($z) != false){
-                        // Сообщение об ошибке.
-						$result = 'Пользователь с таким почтовым адресом уже зарегистрирован';
-                    }else{
-                        // Сообщение об ошибке.
-                        $result = 'Ошибка в запросе к БД';
-                    }
-					echo json_encode($result);
-                    //Останавливаем  скрипт
-                    exit();
-                }
-            }else{
-                // Сообщение об ошибке. 
-                $result = 'Укажите Ваш email';
+			
+			if (empty($user->email)) {
+				// Сообщение об ошибке. 
+				$result = 'Укажите Ваш email';
 				echo json_encode($result);
-                    
-                //Останавливаем  скрипт
-                exit();
-            }
-        }else{
-            // Сообщение об ошибке.
-            $result = 'Отсутствует поле для ввода Email';
-			echo json_encode($result);
-                
-            //Останавливаем  скрипт
-            exit();
-        }
+				//Останавливаем скрипт
+				exit();
+			}
+			
+			if (!preg_match("/^[a-z0-9][a-z0-9\._-]*[a-z0-9]*@([a-z0-9]+([a-z0-9-]*[a-z0-9]+)*\.)+[a-z]+/i", $user->email)) {
+				// Сообщение об ошибке. 
+				$result = 'Вы ввели неправельный email';
+				echo json_encode($result);
+				//Останавливаем  скрипт
+				exit();
+			}
+			
+			Message::recursion($jsonArray, $user);
+			if ($user->e >= 1) {
+				// Сообщение об ошибке. 
+				$result = 'Пользователь с таким почтовым адресом уже зарегистрирован';
+				echo json_encode($result);
+				//Останавливаем  скрипт
+				exit();
+			}
 			
 		// (4) Confirm_password
 			
-		if(isset($_POST["confirm_password"])){
-			//Обрезаем пробелы с начала и с конца строки
-            $confirm_password = trim($_POST["confirm_password"]);
-			$password = trim($_POST["password"]);
-
-            if(!empty($confirm_password)){
-				if($confirm_password === $password){
-				
-				}else{
-					// Сообщение об ошибке.
-					$result = 'Пароль и повторный пароль не совпадают';
-					echo json_encode($result);
-						
-					//Останавливаем  скрипт
-					exit();
-				}
-            }else{
-                // Сообщение об ошибке. 
-                $result = 'Укажите повторно пароль';
+			if (empty($user->confirm_password)) {
+				// Сообщение об ошибке. 
+				$result = 'Укажите повторно пароль';
 				echo json_encode($result);
-                    
-                //Останавливаем  скрипт
-                exit();
-            }
-        }else{
-            // Сообщение об ошибке. 
-            $result = 'Отсутствует поле для ввода повторного пароля';
-			echo json_encode($result);
-               
-            //Останавливаем  скрипт
-            exit();
-        }
-            
+				//Останавливаем скрипт
+				exit();
+			}
+			
+			if($user->confirm_password !== $user->password){
+				// Сообщение об ошибке.
+				$result = 'Пароль и повторный пароль не совпадают';
+				echo json_encode($result);
+				//Останавливаем  скрипт
+				exit();
+			}
+			
 		// (5) Password
 			
-        if(isset($_POST["password"])){
-            //Обрезаем пробелы с начала и с конца строки
-            $password = trim($_POST["password"]);
-
-            if(!empty($password)){
-                $password = htmlspecialchars($password, ENT_QUOTES);
-				$reg_password = "/^(?=.*[a-z])(?=.*\\d)\w{6,}$/i";
-
-                //Если формат полученного пароля не соответствует регулярному выражению
-                if( !preg_match($reg_password, $password)){
-                    // Сообщение об ошибке. 
-                    $result = 'Пароль не соответствует требованиям. Пароль должен состоять только из цифр и букв, не менее чем из 6 символов, обязательно должны присутствовать как минимум одна цифра и одна буква';
-					echo json_encode($result);
-                       
-                    //Останавливаем  скрипт
-                    exit();
-                }	
-                //Шифруем пароль
-                $password = md5($password."top_secret");
-				
-            }else{
-                // Сообщение об ошибке.
-                $result = 'Укажите Ваш пароль';
+			if (empty($user->password)) {
+				// Сообщение об ошибке. 
+				$result = 'Укажите Ваш пароль';
 				echo json_encode($result);
-                //Останавливаем  скрипт
-                exit();
-            }
-        }else{
-            // Сообщение об ошибке. 
-            $result = 'Отсутствует поле для ввода пароля';
-			echo json_encode($result);
-                
-            //Останавливаем  скрипт
-            exit();
-        }
-
-        // (6) Добавления пользователя в БД
-
-        //Запрос на добавления пользователя в БД
-		$taskList = [
-			'login'=>$login,
-			'password'=>$password,
-			'email'=>$email,
-			'name'=>$name
-		];
+				//Останавливаем скрипт
+				exit();
+			}
 			
-		if ($taskList){
-			$jsonArray[] = $taskList;
-			$result_insert = file_put_contents('db.json', json_encode($jsonArray, JSON_FORCE_OBJECT));
+			if (!preg_match("/^(?=.*[a-z])(?=.*\\d)\w{6,}$/i", $user->password)) {
+				// Сообщение об ошибке. 
+				$result = 'Пароль не соответствует требованиям. Пароль должен состоять только из цифр и букв, не менее чем из 6 символов, обязательно должны присутствовать как минимум одна цифра и одна буква';
+				echo json_encode($result);
+				//Останавливаем  скрипт
+				exit();
+			}
+			
+			Db::create($user->login, $user->password, $user->email, $user->name, $jsonArray);
 		}
-			
-        if(!$result_insert){
-            // Сообщение об ошибке.
-            $result = 'Ошибка запроса на добавления пользователя в БД';
-			echo json_encode($result);
-                
-            //Останавливаем  скрипт
-            exit();
-        }else{
-			$result = 'Регистрация прошла успешно!!! Теперь Вы можете авторизоваться используя Ваш логин и пароль.';
-			echo json_encode($result);
-        }
-    }else{
-		exit("<p><strong>Ошибка!</strong> Вы зашли на эту страницу напрямую, поэтому нет данных для обработки. Вы можете перейти на <a href='index.php'> главную страницу </a>.</p>");
-    }
+	}
 	
+	class Db {
+	
+		public static function create($login, $pass, $email, $name, $jsonArray){
+			//Шифруем пароль
+			$password = md5($pass."top_secret");
+			
+			//Запрос на добавления пользователя в БД
+			$taskList = [
+				'login'=>$login,
+				'password'=>$password,
+				'email'=>$email,
+				'name'=>$name
+			];
+				
+			if ($taskList){
+				$jsonArray[] = $taskList;
+				$result_insert = file_put_contents('db.json', json_encode($jsonArray, JSON_FORCE_OBJECT));
+			}
+				
+			if(!$result_insert){
+				// Сообщение об ошибке.
+				$result = 'Ошибка запроса на добавления пользователя в БД';
+				echo json_encode($result);
+				//Останавливаем  скрипт
+				exit();
+			}else{
+				$result = 'Регистрация прошла успешно!!! Теперь Вы можете авторизоваться используя Ваш логин и пароль.';
+				echo json_encode($result);
+			}
+		}
+	}
+
+	Message::checks();
+  	
 ?>
